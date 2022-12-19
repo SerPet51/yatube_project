fc920075bcf7947bfd41d6d6a587a5f8a165fdf7
Сергей Петров <tetkaslepoka@yandex.ru>
2022-12-19 03:42:41 +0300

    6/6

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('group_list/', views.group_list, name='group_list')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 def group_list(request):
     template = 'posts/group_list.html'
